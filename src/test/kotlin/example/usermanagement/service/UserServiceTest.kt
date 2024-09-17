11732dc1761510d92b8971c15e6318e1c05703b3
Bruno Daniel <brunodp.oliveira@gmail.com>
2024-09-17 14:40:53 -0300

    Fix: Unit test

--- src/test/kotlin/example/usermanagement/service/UserServiceTest.kt
+++ src/test/kotlin/example/usermanagement/service/UserServiceTest.kt
@@ -7,13 +7,11 @@ import io.mockk.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-// import org.springframework.kafka.core.KafkaTemplate
 
 class UserServiceTest {
 
     private lateinit var userRepository: UserRepository
     private lateinit var inMemoryQueueService: InMemoryQueueService
-    // private lateinit var kafkaTemplate: KafkaTemplate<String, String>
     private lateinit var meterRegistry: SimpleMeterRegistry
     private lateinit var userService: UserServiceImpl
 
@@ -21,7 +19,6 @@ class UserServiceTest {
     fun setup() {
         userRepository = mockk()
         inMemoryQueueService = mockk(relaxed = true)
-        // kafkaTemplate = mockk(relaxed = true)
         meterRegistry = SimpleMeterRegistry()
         userService = UserServiceImpl(userRepository, inMemoryQueueService, meterRegistry)
     }
@@ -30,7 +27,10 @@ class UserServiceTest {
     fun `createUser should save user, send notification, and increment metric`() {
         // Given
         val user = User(name = "John Doe", email = "john@example.com")
-        every { userRepository.save(any()) } returns user.copy(id = 1)
+        val savedUser = user.copy(id = 1)
+
+        every { userRepository.findByEmail(user.email) } returns null
+        every { userRepository.save(any()) } returns savedUser
 
         // When
         val result = userService.createUser(user)
@@ -40,16 +40,11 @@ class UserServiceTest {
         assertEquals("John Doe", result.name)
         assertEquals("john@example.com", result.email)
 
-        verify(exactly = 1) { userRepository.save(any()) }
+        verify(exactly = 1) { userRepository.findByEmail(user.email) }
+        verify(exactly = 1) { userRepository.save(user) }
         verify(exactly = 1) {
             inMemoryQueueService.sendMessage("New user created: John Doe (john@example.com)")
         }
-        // verify(exactly = 1) {
-        //     kafkaTemplate.send(
-        //         "user-creation-topic",
-        //         "New user created: John Doe (john@example.com)"
-        //     )
-        // }
 
         assertEquals(1.0, meterRegistry.counter("user.created").count(), 0.01)
     }
